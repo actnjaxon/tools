6f33e431d55927b2d1991dd8228a5eae465433c6
Jackson Sweeney <jackson.sweeney@gmail.com>
2019-05-04 00:15:26 -0400

    Updating with proper cmdlet formating

--- networking/set-dhcp.ps1
+++ networking/set-dhcp.ps1
@@ -1,13 +1,44 @@
-#The reverse of set-static
-#This rolls you back to useing DHCP
-#NOTE THIS WILL RUN AGAINS ALL ACTIVE ADAPTERS, DON'T USE ON A SERVER UNLESS YOU FILTER THE OUTPUT FIRST
+<#
+.SYNOPSIS
+  Sets ALL IP enabled network adapters on a given PC to use DHCP
+.DESCRIPTION
+  This script will use CIM to step though each IP enabled network adapter on a computer then configure it for DHCP.
+.EXAMPLE
+  PS C:\> set-dhcp
+  Configures the network adapters on the local system to use DHCP.
+.EXAMPLE
+  PS C:\> set-dhcp -computername PC1,PC2,...
+  PC C:\> PC1,PC2 | set-dhcp
+  Configures the network adapters on remote PCs to use DHCP
 
-Function Set-DHCP {
-  param(
-    [String]$ComputerName = "."
-  )
-  $net = Get-WmiObject -class "Win32_NetworkAdapterConfiguration" -computername $ComputerName | Where-Object {$_.IPEnabled -Match "True"}
+.INPUTS
+  System.String[]
+.OUTPUTS
+  none
+.NOTES
+  This command is a bit of a blunt implement. Since it's using Win32_NetworkAdapterConfiguration it can't easily filter for a specific network adapter.
+#>
+
+[CmdletBinding()]
+param(
+  [parameter(Mandatory=$False,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+    [String[]]
+    [Alias("ServerName", "cn")]
+    $Computername = $null
+)
+
+BEGIN{
+
+}
+
+PROCESS{
+  $net = Get-CimInstance -ClassName "Win32_NetworkAdapterConfiguration" -computername $Computername -Filter "IPEnabled=$True"
   Foreach ($adapter in $net) {
-    adapter.setDHCP() > $null
+    adapter.setDHCP > $null
   }
+}
+END{
+
 }
